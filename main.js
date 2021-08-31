bd9a6dd5b67013fb5344aa312e1c9c2a008931b1
Johan Castillo <jcjohan2707@gmail.com>
2021-08-31 10:48:55 -0400

    Add animation

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 let videoMobile = document.getElementById("banner-video-mobile")
 let videoDesktop = document.getElementById("banner-video-desktop")
 let preloader = document.getElementById("preloader-box")
+let imgAbout = document.getElementById("img-about")
 
 document.body.style.overflow = "hidden"
 
@@ -20,5 +21,22 @@ addEventListener("DOMContentLoaded", () => {
         preloader.style.display = "none";
         document.body.style.overflow = "auto"        
     }, 1200)
+
+})
+
+addEventListener("scroll", () => {
+    
+    let scroll = window.scrollY
+
+    if(scroll > 430){
+        imgAbout.classList.add("animate__fadeInRight")
+    }else{
+        imgAbout.classList.remove("animate__fadeInRight")
+    }
+
+    
+
+
+    console.log(scroll)
 
 })
